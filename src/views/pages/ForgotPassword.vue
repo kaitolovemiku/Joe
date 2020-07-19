d37584e9783ed557a252893d3eda83720e377879
Polawat Huang (Joe) <polawat_crazy@hotmail.com>
2020-07-19 18:35:25 +0700

    update forget password system

--- src/views/pages/ForgotPassword.vue
+++ src/views/pages/ForgotPassword.vue
@@ -1,86 +1,178 @@
 <template>
-  <CContainer class="d-flex align-items-center min-vh-100">
-    <CRow class="w-100 justify-content-center">
-      <CCol md="12">
-        <section class="section">
-          <div class="container">
-            <div class="columns">
-              <div class="column is-8 is-offset-2">
-                <horizontal-stepper
-                  :steps="demoSteps"
-                  @completed-step="completeStep"
-                  @active-step="isStepActive"
-                  @stepper-finished="alert"
-                ></horizontal-stepper>
-              </div>
-            </div>
-          </div>
-        </section>
-      </CCol>
-      <CCol md="12" style="text-align:center;">
-          <CButton color="link" v-on:click="goBackToLogin()" class="px-0">Go back to login</CButton>
-      </CCol>
-    </CRow>
-  </CContainer>
+  <div class="d-flex align-items-center min-vh-100">
+    <CContainer fluid>
+      <CRow class="justify-content-center">
+        <CCol md="6">
+          <CCard class="mx-4 mb-0">
+            <CCardBody class="p-4">
+              <CForm v-if="!checkValidate">
+                <h1>Forgot password</h1>
+                <p
+                  class="text-muted"
+                >Input your email and answer your password security question to reset a new password.</p>
+                <CInput v-model="email" placeholder="Email" autocomplete="email" prepend="@" />
+                <div class="input-group mb-3">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon1">
+                      <i class="fa fa-question"></i>
+                    </span>
+                  </div>
+                  <select class="form-control" v-model="questionTarget">
+                    <option disabled value>Choice your question for password security</option>
+                    <option v-for="option in questions" v-bind:key="option.id">{{ option.data }}</option>
+                  </select>
+                </div>
+                <CInput
+                  v-model="answer"
+                  placeholder="Answer the question what's your choice."
+                  autocomplete="yes"
+                >
+                  <template #prepend-content>
+                    <CIcon name="cil-lock-locked" />
+                  </template>
+                </CInput>
+
+                <CButton @click="checkUserData()" color="success" block>Create Account</CButton>
+              </CForm>
+              <CForm v-if="checkValidate">
+                <h1>Reset a new password</h1>
+                <p
+                  class="text-muted"
+                >Almost done, Reset your new password to login in the next time.</p>
+                <CInput
+                  v-model="password"
+                  placeholder="New Password"
+                  type="password"
+                  autocomplete="new-password"
+                >
+                  <template #prepend-content>
+                    <CIcon name="cil-lock-locked" />
+                  </template>
+                </CInput>
+                <CInput
+                  v-model="repassword"
+                  placeholder="Confirm Password"
+                  type="password"
+                  autocomplete="new-password"
+                >
+                  <template #prepend-content>
+                    <CIcon name="cil-lock-locked" />
+                  </template>
+                </CInput>
+
+                <CButton @click="saveData()" color="success" block>Reset password</CButton>
+              </CForm>
+            </CCardBody>
+            <CCardFooter class="p-4">
+              <CRow>
+                <CCol col="12">
+                  <CButton block @click="goBackToLogin()" color="twitter">Go back to login</CButton>
+                </CCol>
+              </CRow>
+            </CCardFooter>
+          </CCard>
+        </CCol>
+      </CRow>
+    </CContainer>
+  </div>
 </template>
 
 <script>
-import HorizontalStepper from "vue-stepper";
-import StepOne from "./ForgotPasswordStepOne";
-import StepTwo from "./ForgotPasswordStepTwo";
+import firebase from "firebase";
+const db = firebase.firestore();
 
 export default {
   name: "ForgotPassword",
-  components: {
-    HorizontalStepper
-  },
   data() {
     return {
-      demoSteps: [
-        {
-          icon: "mail",
-          name: "first",
-          title: "Answer a question",
-          subtitle: "Answer your personal question to make sure that is you.",
-          component: StepOne,
-          completed: false
-        },
-        {
-          icon: "report_problem",
-          name: "second",
-          title: "Change password",
-          subtitle: "Set your new password",
-          component: StepTwo,
-          completed: false
-        }
-      ]
+      username: "",
+      email: "",
+      handPhone: "",
+      userId: "",
+      targetUser: [],
+      checkValidate: false,
+      companyPhone: "",
+      roleTarget: "",
+      address: "",
+      users: [],
+      answer: "",
+      role: ["teacher", "student"],
+      questions: [{ id: 1, data: "Waiting for database loading." }],
+      password: "",
+      repassword: "",
+      questionTarget: "",
+      uploadValue: 0,
+      profilePhoto: null,
+      userPhoto: null,
+      photoUrl: "",
+      userData: "",
+      bio: ""
     };
   },
+  created() {
+    db.collection("questions")
+      .get()
+      .then(querySnapshot => {
+        this.questions = [];
+        querySnapshot.forEach(doc => {
+          return this.questions.push({
+            id: doc.id,
+            data: doc.data().questionDetail
+          });
+        });
+      });
+    db.collection("users")
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          return this.users.push({ id: doc.id, data: doc.data() });
+        });
+      })
+      .catch(error => {
+        console.log("Error getting user data: ", error);
+      });
+  },
   methods: {
+    checkUserData() {
+      const target = this.users.find(item => {
+        return item.data.email == this.email;
+      });
+
+      if (target !== undefined) {
+        if (
+          target.data.questionId == this.questionTarget &&
+          target.data.questionAns == this.answer
+        ) {
+          this.checkValidate = true;
+          this.targetUser.push(target);
+        } else {
+          window.alert("Sorry, your question or answer is incorrect!");
+        }
+      } else {
+        window.alert("Sorry, we have no this email in database!");
+      }
+    },
+    saveData() {
+      if (this.password == this.repassword) {
+        db.collection("users")
+          .doc(this.targetUser[0].id)
+          .update({
+            password: this.password
+          });
+          let check = confirm("Update password successed. Do you want to login?");
+
+          if(check == true) {
+            this.$router.replace({ name: "Login" });
+          }
+
+      } else {
+        window.alert(
+          "Sorry, the new password and confirmation password do not match!"
+        );
+      }
+    },
     goBackToLogin() {
       this.$router.replace({ name: "Login" });
-    },
-    // Executed when @completed-step event is triggered
-    completeStep(payload) {
-      this.demoSteps.forEach(step => {
-        if (step.name === payload.name) {
-          step.completed = true;
-        }
-      });
-    },
-    // Executed when @active-step event is triggered
-    isStepActive(payload) {
-      this.demoSteps.forEach(step => {
-        if (step.name === payload.name) {
-          if (step.completed === true) {
-            step.completed = false;
-          }
-        }
-      });
-    },
-    // Executed when @stepper-finished event is triggered
-    alert(payload) {
-      alert(payload);
     }
   }
 };
--- src/views/pages/ForgotPasswordStepOne.vue
+++ src/views/pages/ForgotPasswordStepOne.vue
@@ -1,129 +1,243 @@
 <template>
-  <div style="padding: 2rem 3rem; text-align: justify;">
-    <div class="field">
-      <div class="control">
-        <CInput
-          v-model="form.demoEmail"
-          :class="['input', ($v.form.demoEmail.$error) ? 'is-danger' : '']"
-          label="Email"
-          type="email"
-          placeholder="Enter your email"
-        />
-      </div>
-      <p v-if="$v.form.demoEmail.$error" class="help is-danger">This email is invalid</p>
-    </div>
-    <div class="field">
-      <label class="label">Question</label>
-      <div class="control">
-        <select v-model="form.question" :class="['form-control', ($v.form.demoEmail.$error) ? 'is-danger' : '']">
-          <option disabled value>the question that you created when you built this account.</option>
-          <option v-for="option in questions" v-bind:key="option['id']" >{{option['data']}}</option>
-        </select>
-      </div>
-      <p v-if="$v.form.question.$error" class="help is-danger">This question is invalid</p>
-    </div>
-    <div class="field">
-      <div class="control">
-        <CInput
-          v-model="form.message"
-          :class="['CInput', ($v.form.message.$error) ? 'is-danger' : '']"
-          label="Input your answer"
-          type="text"
-          placeholder="Enter your answer"
-        />
-      </div>
-      <p v-if="$v.form.message.$error" class="help is-danger">This answer is invalid</p>
-    </div>
+  <div class="d-flex align-items-center min-vh-100">
+    <CContainer fluid>
+      <CRow class="justify-content-center">
+        <CCol md="6">
+          <CCard class="mx-4 mb-0">
+            <CCardBody class="p-4">
+              <CForm>
+                <h1>Forgot password</h1>
+                <p class="text-muted">Create your account</p>
+                <CInput v-model="username" placeholder="Username" autocomplete="username">
+                  <template #prepend-content>
+                    <CIcon name="cil-user" />
+                  </template>
+                </CInput>
+                <CInput
+                  v-model="userId"
+                  placeholder="Student ID or worker ID"
+                  autocomplete="username"
+                >
+                  <template #prepend-content>
+                    <CIcon name="cil-user" />
+                  </template>
+                </CInput>
+                <CInput v-model="address" placeholder="Address" autocomplete="address">
+                  <template #prepend-content>
+                    <i class="fa fa-address-card-o" aria-hidden="true"></i>
+                  </template>
+                </CInput>
+                <CInput v-model="handPhone" placeholder="Phone" autocomplete="0881112222">
+                  <template #prepend-content>
+                    <i class="fa fa-phone"></i>
+                  </template>
+                </CInput>
+                <CInput
+                  v-model="companyPhone"
+                  placeholder="Company phone (if any)"
+                  autocomplete="0881112222"
+                >
+                  <template #prepend-content>
+                    <i class="fa fa-phone"></i>
+                  </template>
+                </CInput>
+                <CInput
+                  v-model="bio"
+                  placeholder="Job position"
+                  autocomplete="Student of Software Engineer"
+                >
+                  <template #prepend-content>
+                    <i class="fa fa-book"></i>
+                  </template>
+                </CInput>
+
+                <div class="input-group mb-3">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon1">
+                      <i class="fa fa-flag-o"></i>
+                    </span>
+                  </div>
+                  <select class="form-control" v-model="roleTarget">
+                    <option disabled value>Role</option>
+                    <option v-for="option in role" v-bind:key="option">{{ option }}</option>
+                  </select>
+                </div>
+                <div class="input-group mb-3">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon1">
+                      <i class="fa fa-question"></i>
+                    </span>
+                  </div>
+                  <select class="form-control" v-model="questionTarget">
+                    <option disabled value>Choice your question for password security</option>
+                    <option v-for="option in questions" v-bind:key="option.id">{{ option.data }}</option>
+                  </select>
+                </div>
+                <CInput
+                  v-model="answer"
+                  placeholder="Answer the question what's your choice."
+                  autocomplete="yes"
+                >
+                  <template #prepend-content>
+                    <CIcon name="cil-lock-locked" />
+                  </template>
+                </CInput>
+                <CInput v-model="email" placeholder="Email" autocomplete="email" prepend="@" />
+                <CInput
+                  v-model="password"
+                  placeholder="Password"
+                  type="password"
+                  autocomplete="new-password"
+                >
+                  <template #prepend-content>
+                    <CIcon name="cil-lock-locked" />
+                  </template>
+                </CInput>
+                <div class="input-group mb-3">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroupFileAddon01">Upload your picture</span>
+                  </div>
+                  <div class="custom-file">
+                    <input
+                      @change="previewFile"
+                      type="file"
+                      class="custom-file-input"
+                      id="inputGroupFile01"
+                      aria-describedby="inputGroupFileAddon01"
+                    />
+                    <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+                  </div>
+                </div>
+                <CProgress
+                  class="mb-3"
+                  :value="uploadValue"
+                  :min="0"
+                  :max="100"
+                  show-percentage
+                  animated
+                ></CProgress>
+                <CButton @click="saveData()" color="success" block>Create Account</CButton>
+              </CForm>
+            </CCardBody>
+            <CCardFooter class="p-4">
+              <CRow>
+                <CCol col="12">
+                  <CButton block @click="goBackToLogin()" color="twitter">Go back to login</CButton>
+                </CCol>
+              </CRow>
+            </CCardFooter>
+          </CCard>
+        </CCol>
+      </CRow>
+    </CContainer>
   </div>
 </template>
 
 <script>
-import { validationMixin } from "vuelidate";
-import { required, email } from "vuelidate/lib/validators";
 import firebase from "firebase";
-
 const db = firebase.firestore();
 
 export default {
-  name: "ForgotPassStepOne",
-  props: ["clickedNext", "currentStep"],
-  mixins: [validationMixin],
+  name: "ForgotPassword",
   data() {
     return {
-      questionchoice: "",
+      username: "",
+      email: "",
+      handPhone: "",
+      userId: "",
+      companyPhone: "",
+      roleTarget: "",
+      address: "",
       users: [],
-      questions: [],
-      form: {
-        question: "",
-        demoEmail: "",
-        message: ""
-      }
+      answer: "",
+      role: ["teacher", "student"],
+      questions: [{ id: 1, data: "Waiting for database loading." }],
+      password: "",
+      questionTarget: "",
+      uploadValue: 0,
+      profilePhoto: null,
+      userPhoto: null,
+      photoUrl: "",
+      userData: "",
+      bio: ""
     };
   },
   created() {
+    db.collection("questions")
+      .get()
+      .then(querySnapshot => {
+        this.questions = [];
+        querySnapshot.forEach(doc => {
+          return this.questions.push({
+            id: doc.id,
+            data: doc.data().questionDetail
+          });
+        });
+      });
     db.collection("users")
       .get()
       .then(querySnapshot => {
         querySnapshot.forEach(doc => {
-          this.users.push(doc.data());
+          return this.users.push({ id: doc.id, data: doc.data() });
         });
+      })
+      .catch(error => {
+        console.log("Error getting user data: ", error);
       });
-    db.collection("questions")
-      .get()
-      .then(querySnapshot => {
-        querySnapshot.forEach(doc => {
-          this.questions.push({ id: doc.id, data: doc.data().questionDetail });
-        });
-      });
-    console.log(this.users, this.questions);
-  },
-  validations: {
-    form: {
-      question: {
-        required,
-        unique: val => {
-          
-          if (val === '') return true
-          return window.alert(val)
-        }
-      },
-      demoEmail: {
-        required,
-        email
-      },
-      message: {
-        required
-      }
-    }
   },
   methods: {
-    test(event) {
-      this.form.question = JSON.stringify(event.target.value);
-      console.log(this.form.question);
-    }
-  },
-  watch: {
-    $v: {
-      handler: function(val) {
-        if (!val.$invalid) {
-          this.$emit("can-continue", { value: true });
-        } else {
-          this.$emit("can-continue", { value: false });
-        }
-      },
-      deep: true
+    previewFile(e) {
+      this.uploadValue = 0;
+      this.profilePhoto = null;
+      this.userPhoto = e.target.files[0];
     },
-    clickedNext(val) {
-      if (val === true) {
-        this.$v.form.$touch();
-      }
-    }
-  },
-  mounted() {
-    if (!this.$v.$invalid) {
-      this.$emit("can-continue", { value: true });
-    } else {
-      this.$emit("can-continue", { value: false });
+    saveData() {
+      db.collection("users").add({
+        address: this.address,
+        companyPhone: this.companyPhone,
+        createdAt: new Date(),
+        email: this.email,
+        handPhone: this.handPhone,
+        jobPosition: this.bio,
+        password: this.password,
+        photo: this.userPhoto.name,
+        questionAns: this.answer,
+        questionId: this.questionTarget,
+        role: this.roleTarget == "student" ? "guest" : this.roleTarget,
+        status: "online",
+        userId: this.userId,
+        username: this.username
+      });
+      const storageRef = firebase
+        .storage()
+        .ref("images")
+        .child(`${this.userPhoto.name}`)
+        .put(this.userPhoto);
+      storageRef.on(
+        `state_changed`,
+        snapshot => {
+          this.uploadValue =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        },
+        error => {
+          console.log(error.message);
+        },
+        () => {
+          this.uploadValue = 100;
+        }
+      );
+      firebase
+        .storage()
+        .ref("images")
+        .child(this.userPhoto.name)
+        .getDownloadURL()
+        .then(url => {return console.log(url),window.alert("Upload successed"),this.$router.replace({ name: "Login" })})
+        .catch(error => {
+          console.log("Error getting image data: ", error);
+        });
+    },
+    goBackToLogin() {
+      this.$router.replace({ name: "Login" });
     }
   }
 };
