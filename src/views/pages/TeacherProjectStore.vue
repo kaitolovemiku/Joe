4493895aaf1ef438671caa2e77b22a591b54ed56
Polawat Huang (Joe) <polawat_crazy@hotmail.com>
2020-07-21 11:48:10 +0700

    update project detail

--- src/views/pages/TeacherProjectStore.vue
+++ src/views/pages/TeacherProjectStore.vue
@@ -17,9 +17,16 @@
       </CCardHeader>
       <CCardBody>
         <div class="input-group mb-3">
-          <multiselect v-model="search" :options="options" placeholder="Input a project name ..." style="width:90%;"></multiselect>
+          <multiselect
+            v-model="search"
+            :options="options"
+            placeholder="Input a project name ..."
+            style="width:90%;"
+          ></multiselect>
           <div class="input-group-append" style="width:10%;">
-            <button v-on:click="searchProject()" class="btn btn-info" type="button"><CIcon name="cil-magnifying-glass"/> Search</button>
+            <button v-on:click="searchProject()" class="btn btn-info" type="button">
+              <CIcon name="cil-magnifying-glass" />Search
+            </button>
           </div>
         </div>
       </CCardBody>
@@ -49,6 +56,7 @@
 import Multiselect from "vue-multiselect";
 import firebase from "firebase/app";
 import "firebase/firestore";
+import Vue from "vue";
 
 const db = firebase.firestore();
 
@@ -70,19 +78,24 @@ export default {
       .get()
       .then(querySnapshot => {
         querySnapshot.forEach(doc => {
-          this.projects.push({
-            id: doc.id,
-            projectNameEn: doc.data().projectNameEn,
-            projectNameTh: doc.data().projectNameTh,
-            projectBg: doc.data().projectBg,
-            projectType: doc.data().projectType,
-            projectDuration: doc.data().projectDuration,
-            projectPoint: doc.data().projectPoint,
-            projectStatus: doc.data().projectStatus
-          });
+          if (
+            doc.data().projectAdvisor.includes(Vue.prototype.$session.getAll().user.id) === true
+          ) {
+            this.projects.push({
+              id: doc.id,
+              projectNameEn: doc.data().projectNameEn,
+              projectNameTh: doc.data().projectNameTh,
+              projectBg: doc.data().projectBg,
+              projectType: doc.data().projectType,
+              projectDuration: doc.data().projectDuration,
+              projectPoint: doc.data().projectPoint,
+              projectStatus: doc.data().projectStatus
+            });
+          }
           console.log(doc.id, " => ", doc.data());
+          console.log("Advisor->", Vue.prototype.$session.getAll().user);
         });
-        return this.searchObject = this.projects;
+        return (this.searchObject = this.projects);
       })
       .catch(error => {
         console.log("Error getting documents: ", error);
@@ -91,34 +104,40 @@ export default {
       .get()
       .then(querySnapshot => {
         querySnapshot.forEach(doc => {
-          this.options.push(doc.data().projectName);
+          this.options.push(doc.data().projectNameTh);
+          this.options.push(doc.data().projectNameEn);
         });
       });
   },
   methods: {
-    searchProject () {
+    searchProject() {
       this.searchObject = [];
-      if(this.search) {
-      this.projects.forEach(doc => {
-        if(this.search == doc.projectName){
-          this.searchObject.push(doc);
-        }
-      });
+      if (this.search) {
+        this.projects.forEach(doc => {
+          if (this.search == doc.projectNameTh || this.search == doc.projectNameEn) {
+            this.searchObject.push(doc);
+          }
+        });
       } else {
         this.searchObject = this.projects;
       }
       return this.searchObject;
     },
     routePage(id) {
-      this.$router.replace('/menu/projectCheckingSystem/'+id);
+      this.$router.replace("/menu/projectCheckingSystem/" + id);
     },
-    getBadge (status) {
+    getBadge(status) {
       switch (status) {
-        case 'Active': return 'success'
-        case 'Inactive': return 'secondary'
-        case 'Pending': return 'warning'
-        case 'Banned': return 'danger'
-        default: 'primary'
+        case "Active":
+          return "success";
+        case "Inactive":
+          return "secondary";
+        case "Pending":
+          return "warning";
+        case "Banned":
+          return "danger";
+        default:
+          "primary";
       }
     }
   }
@@ -133,7 +152,7 @@ export default {
 }
 .zoom:hover {
   box-shadow: 2px 2px rgba(65, 60, 60, 0.678);
-  transition: transform .25s, visibility .25s ease-in;
+  transition: transform 0.25s, visibility 0.25s ease-in;
 }
 </style>
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
